25630c4b609dd359ba853588eac2a60ae6d68d22
7AiRzzZ <maxencedpr@icloud.com>
2023-09-04 11:23:37 +0200

    update

--- src/app/home/sections/introduction/index.jsx
+++ src/app/home/sections/introduction/index.jsx
@@ -11,7 +11,7 @@ const Introduction = () => {
               <h1 data-aos="fade-right" className="text-xl lg:text-6xl">Virtus</h1>
               <span data-aos="fade-left" className="text-bluelight text-xl lg:text-6xl">DEV</span>
             </div>
-            <p data-aos="fade-up"className="mx-10 text-sm lg:px-40 lg:border-x lg:border-x-blue4/50 sm:text-xs sm:py-10">
+            <p data-aos="fade-up"className="mx-10 my-4 text-sm lg:px-40 lg:border-x lg:border-x-blue4/50 sm:text-xs sm:py-10">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero,
               cumque? Maiores, fugit magnam! Unde molestiae tempore tenetur enim
               cumque perferendis ad ut consectetur provident obcaecati dolorum
